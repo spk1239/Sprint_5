5d613f8a7307010e86a2ef6eeafa5848b38b2fa1
Birukov Nikita <birukov.na@yandex.ru>
2025-09-15 22:39:55 +0300

    first commit

---
+++ tests/tests_main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+import pytest
